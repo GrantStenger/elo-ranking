6803cdea51bef74711f6bf23f6eb8369cf1b8b27
gstenger98 <gstenger98@gmail.com>
2018-07-01 23:52:31 -0700

    rank actually works now

--- rank.py
+++ rank.py
@@ -5,90 +5,74 @@ import os
 
 # def expected_score(rating_A, rating_B):
 # 	return 1 / (1 + 10**((rating_B-rating_A)/400))
+def get_win_loss(phil):
+	philosophers[phil][0] / (philosophers[phil][0] + philosophers[phil][1])
 
 def expected_score(phil_1, phil_2):
-	return 1 / (1 + 10**((rating_B-rating_A)/400))
+	if philosophers[phil_1][0] + philosophers[phil_1][1] > 0:
+		w_l_score_1 = get_win_loss(phil_1)
+	else:
+		w_l_score_1 = 0.5
+
+	if philosophers[phil_2][0] + philosophers[phil_2][1]:
+		w_l_score_2 = get_win_loss(phil_2)
+	else:
+		w_l_score_2 = 0.5
+
+	return 1 / (1 + 10**((w_l_score_1-w_l_score_2)/400))
 
 def new_score(prev_score, tournament_score, expected_score):
 	return prev_score + 32 * (tournament_score - expected_score)
 
-def print_rankings(philosophers):
-	print("Current Rankings: ")
-
-	sorted_dict = sorted(philosophers.items(), key=operator.itemgetter(1))
-	sorted_dict.reverse()
-	for phil in sorted_dict:
-		print(phil[0] + ": " + str(phil[1]))
+def sort_and_print(philosophers):
+	sorted_list = sorted(philosophers, key=lambda phil: philosophers[phil][2])
+	print("Rankings: ")
+	for i in range(len(sorted_list)):
+		phil = sorted_list[len(sorted_list) - i - 1]
+		print(phil + ": " + str(philosophers[phil][0]) + " wins, " + str(philosophers[phil][1]) + " losses")
 	print()
 	print()
-
-def insertionSort(philosophers):
-	for i in range(1,len(philosophers)):
-		wins = philosophers[list(philosophers)[i]][0]
-		losses = philosophers[list(philosophers)[i]][1]
-		if wins + losses > 0:
-			curr_value = wins / (wins + losses)
-		else:
-			curr_value = 0.5
-
-		print("current value:", curr_value)
-
-		# if i > 0:
-		# 	if philosophers[list(philosophers)[i-1]][0] + philosophers[list(philosophers)[i-1]][1] > 0:
-		# 		prev_value = philosophers[list(philosophers)[0]][0]/(philosophers[list(philosophers)[0]][0]+philosophers[list(philosophers)[0]][1])
-		# 	else:
-		# 		prev_value = 0.5
-		
-		position = i
-		possition_w_l = 0.5
-		# possition_w_l = philosophers[list(philosophers)[position-1]][0]/(philosophers[list(philosophers)[position-1]][0]+philosophers[list(philosophers)[position-1]][1])
-		while position > 0 and possition_w_l > curr_value:
-			philosophers[list(philosophers)[position]] = philosophers[list(philosophers)[position-1]]
-			position -= 1
-			curr_value = philosophers[list(philosophers)[position]][0]/(philosophers[list(philosophers)[position]][0]+philosophers[list(philosophers)[position]][1])
-			possition_w_l = philosophers[list(philosophers)[position-1]][0]/(philosophers[list(philosophers)[position-1]][0]+philosophers[list(philosophers)[position-1]][1])
+	return True
 
 # "Name": (wins, losses)
 philosophers = {
-	"Hume" : (0, 0),
-	"Kant" : (0, 0),
-	"Nietzsche" : (0, 0),
-	"Hobbes" : (0, 0),
-	"Rousseau" : (0, 0),
-	"Adam Smith" : (0, 0),
-	"Cicero" : (0, 0),
-	"Deleuze" : (0, 0),
-	"John Stuart Mill" : (0, 0),
-	"Karl Popper" : (0, 0),
-	"Bertrand Russell" : (0, 0),
-	"Schopenhauer" : (0, 0),
-	"Marx" : (0, 0),
-	"Descartes" : (0, 0),
-	"Wittgenstein" : (0, 0),
-	"Kierkegaard" : (0, 0),
-	"Confucius" : (0, 0),
-	"Spinoza" : (0, 0),
-	"John Rawls" : (0, 0),
-	"Sartre" : (0, 0),
-	"Heidegger" : (0, 0),
-	"Frege" : (0, 0),
-	"Derrida" : (0, 0),
-	"Foucault" : (0, 0),
-	"Simone de Beauvoir" : (0, 0),
-	"Machiavelli" : (0, 0),
-	"Bacon" : (0, 0),
-	"Bentham" : (0, 0),
-	"Buddha" : (0, 0),
-	"Noam Chomsky" : (0, 0),
-	"Voltaire" : (0, 0),
-	"Leibniz" : (0, 0),
-	"Hegel" : (0, 0)
+	"Hume" : (0, 0, 0),
+	"Kant" : (0, 0, 0),
+	"Nietzsche" : (0, 0, 0),
+	"Hobbes" : (0, 0, 0),
+	"Rousseau" : (0, 0, 0),
+	"Adam Smith" : (0, 0, 0),
+	"Cicero" : (0, 0, 0),
+	"Deleuze" : (0, 0, 0),
+	"John Stuart Mill" : (0, 0, 0),
+	"Karl Popper" : (0, 0, 0),
+	"Bertrand Russell" : (0, 0, 0),
+	"Schopenhauer" : (0, 0, 0),
+	"Marx" : (0, 0, 0),
+	"Descartes" : (0, 0, 0),
+	"Wittgenstein" : (0, 0, 0),
+	"Kierkegaard" : (0, 0, 0),
+	"Confucius" : (0, 0, 0),
+	"Spinoza" : (0, 0, 0),
+	"John Rawls" : (0, 0, 0),
+	"Sartre" : (0, 0, 0),
+	"Heidegger" : (0, 0, 0),
+	"Frege" : (0, 0, 0),
+	"Derrida" : (0, 0, 0),
+	"Foucault" : (0, 0, 0),
+	"Simone de Beauvoir" : (0, 0, 0),
+	"Machiavelli" : (0, 0, 0),
+	"Bacon" : (0, 0, 0),
+	"Bentham" : (0, 0, 0),
+	"Buddha" : (0, 0, 0),
+	"Noam Chomsky" : (0, 0, 0),
+	"Voltaire" : (0, 0, 0),
+	"Leibniz" : (0, 0, 0),
+	"Hegel" : (0, 0, 0)
 }
 
-insertionSort(philosophers)
-print(philosophers)
-
-print(list(philosophers)[0])
+# sort(philosophers)
+# print(philosophers)
 
 pairings = []
 count = 0
@@ -112,7 +96,9 @@ count = 0
 
 while quit == False and count < length:
 	os.system('clear')
-	print_rankings(philosophers)
+	sort_and_print(philosophers)
+	# print_rankings(philosophers)
+
 	phil_1 = shuffled_pairings[count][0]
 	phil_2 = shuffled_pairings[count][1]
 	print(phil_1, "vs.", phil_2)
@@ -129,15 +115,27 @@ while quit == False and count < length:
 	print("The winner is", winner)
 	# print()
 	if winner == phil_1:
-		philosophers[phil_1] = new_score(philosophers[phil_1], 1, expected_score(phil_1, phil_2))
-		philosophers[phil_2] = new_score(philosophers[phil_2], 0, expected_score(philosophers[phil_2], philosophers[phil_1]))
-		# print("New", phil_1, "score", philosophers[phil_1])
-		# print("New", phil_2, "score", philosophers[phil_2])
+		phil_1_wins = philosophers[phil_1][0] + 1
+		phil_1_losses = philosophers[phil_1][1]
+		phil_1_w_l = (philosophers[phil_1][2] + 1) * 1.00001
+		philosophers[phil_1] = (phil_1_wins, phil_1_losses, phil_1_w_l)
+
+		phil_2_wins = philosophers[phil_2][0]
+		phil_2_losses = philosophers[phil_2][1] + 1
+		phil_2_w_l = (philosophers[phil_2][2] - 1) * 1.00001
+		philosophers[phil_2] = (phil_2_wins, phil_2_losses, phil_2_w_l)
+
 	elif winner == phil_2:
-		philosophers[phil_1] = new_score(philosophers[phil_1], 0, expected_score(philosophers[phil_1], philosophers[phil_2]))
-		philosophers[phil_2] = new_score(philosophers[phil_2], 1, expected_score(philosophers[phil_2], philosophers[phil_1]))
-		# print("New", phil_1, "score", philosophers[phil_1])
-		# print("New", phil_2, "score", philosophers[phil_2])
+		phil_1_wins = philosophers[phil_1][0]
+		phil_1_losses = philosophers[phil_1][1] + 1
+		phil_1_w_l = (philosophers[phil_1][2] - 1) * 1.00001
+		philosophers[phil_1] = (phil_1_wins, phil_1_losses, phil_1_w_l)
+
+		phil_2_wins = philosophers[phil_2][0] + 1
+		phil_2_losses = philosophers[phil_2][1]
+		phil_2_w_l = (philosophers[phil_2][2] + 1) * 1.00001
+		philosophers[phil_2] = (phil_2_wins, phil_2_losses, phil_2_w_l)
+
 	else:
 		print("that's not an option u cunt")
 	print()
@@ -149,9 +147,10 @@ while quit == False and count < length:
 		quit = True
 		exit()
 
+os.system('clear')
 print("The Final Ranking Is...")
 print()
-print_rankings(philosophers)
+sort_and_print(philosophers)
 
 # Notes:
 # How to proceed. I need a much more efficient algorithm.
